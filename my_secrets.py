4fc40687aad36dd8f2523271ef747acc3cef858e
jdgibson <jdgibson@mmto.org>
2026-03-31 13:36:27 -0700

    Working on stddev alterr vs ellipticity notebook.

---
+++ my_secrets.py
@@ -0,0 +1,6 @@
+MARIADB_HOST = "db-archive.mmto.arizona.edu"
+DB_MOUNT_HIRES = "mount_hires"
+DB_MEASUREMENTS = "measurements"
+MARIADB_USER = "mmtstaff"
+MARIADB_PASSWORD = "multiple"
+MARIADB_CHARSET = "utf8mb4"  # Use utf8mb4 for full Unicode support
